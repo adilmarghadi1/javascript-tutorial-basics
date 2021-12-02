136529469631ae681c5f0a43ddc6e9cafb1b4647
adilmarghadi1 <adilmarghadi2020@gmail.com>
2021-12-02 11:59:53 +0100

    Javascript tutorial

---
+++ script.js
@@ -0,0 +1,107 @@
+///////////////////////////////////// Javascript Tutorial //////////////////////////////////
+
+
+// let firstName = "Adil ";
+// let lastName = "Dev";
+
+
+// let concat = firstName + lastName
+// alert(concat)
+
+// let num1;
+// let num2;
+// num1 = 20;
+// num2 = 90
+// let sum = num1 + num2
+// alert(sum)
+
+// let num = "abdessamed"
+
+
+
+// for (let i=0; i<100;i++)
+// {
+
+//     document.write("Adil " + i)
+// }
+
+// let i = 0
+
+
+// while( i<=100)
+// {
+//     document.write(i)
+    
+//  i++
+// }
+
+
+
+
+
+
+// let user = prompt("The days of week");
+
+// switch (user)
+// {
+//     case 'monday':
+//         alert ("Have a good week");
+//         break;
+//     case 'thuesday':
+//         alert("happy thuesday")
+//         break;
+// default:
+//     alert("I don't know");
+//     break;
+        
+// }
+
+
+
+// let arr = ['adil', 'kamal', 'yassin', 'karim', 'abdou', 'houssine']
+// arr[4] = 'slimani'
+// arr.push("ahmed")
+// arr.pop()
+// // alert(arr)
+// arr.shift()
+// // alert(arr)
+// arr.unshift("zineb")
+// alert(arr)
+
+
+
+
+// function sum(name, lastName, age) {
+//     let data;
+//     data = "My First Name " + name + "" + "My Last Name is " + lastName + " My Age is " + age;
+//     return data;
+// }
+
+// alert(sum("adil", "dev", 21))
+
+
+// Objects;
+
+
+// let myObj = {
+//     name1:"adil",
+//     lastName : "Dev",
+//     name2:"Houssine",
+//     name3:"kamal",
+//     name4:"karim",
+//     name5:" rania",
+//     name6:"Ahdout",
+//     name7:"faisal",
+// }
+
+// alert(myObj.name2 +  myObj.name5)
+
+
+// let btn = document.getElementById("btn")
+
+// btn.addEventListener("click", function(){
+//     btn.style.backgroundColor = 'red';
+//     btn.textContent = 'Hello';
+//     btn.style.fontSize = '2em';
+//     btn.style.padding = "12px 20px";
+// })
